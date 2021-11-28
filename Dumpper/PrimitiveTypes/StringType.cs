e18df01e319c40db0eede3320c817c9c80a3406c
pritom <ppurkayasta@insightintechnology.com>
2021-11-28 18:35:40 +0600

    Added string extensions

--- Dumpper/PrimitiveTypes/StringType.cs
+++ Dumpper/PrimitiveTypes/StringType.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using Dumpper.Printer;
+
 namespace Dumpper.PrimitiveTypes
 {
     /// <summary>
@@ -7,27 +8,48 @@ namespace Dumpper.PrimitiveTypes
     /// </summary>
     public static class StringType
     {
-        public static void Dumpper(this string strVal)
-        {
+        public static void Dump(this string val) => PrintValues.PrintLine(val);
 
+        public static void Dump(this string val, string startingText) => PrintValues.PrintLine(val, startingText);
+
+        public static void Dump(this string[] arr)
+        {
+            int count = arr.Length;
+            PrintValues.PrintLine($"({count})", "Array Length:");
+            PrintSymbols.PrintBracket("[");
+            for (int i = 0; i < count; i++)
+            {
+                PrintValues.Print(arr[i]);
+
+                if (i < count - 1)
+                    PrintSymbols.PrintComma(", ");
+            }
+            PrintSymbols.PrintBracket("]");
+            PrintSymbols.PrintNewLine();
         }
 
-        public static void Dumpper(this string[] strArrayVal)
+        public static void Dump(this IEnumerable<string> vals)
         {
-
+            vals.ToList().Dump();
         }
 
-        public static void Dumpper(this IEnumerable<string> strings)
+        public static void Dump(this List<string> vals)
         {
+            int count = vals.Count;
+            PrintValues.PrintLine($"({count})", "Collection Length");
+            PrintSymbols.PrintBracket("{");
+            for (int i = 0; i < count; i++)
+            {
+                PrintValues.Print(vals[i]);
 
+                if (i < count - 1)
+                    PrintSymbols.PrintComma(", ");
+            }
+            PrintSymbols.PrintBracket("}");
+            PrintSymbols.PrintNewLine();
         }
 
-        public static void Dumpper(this List<string> strings)
-        {
-
-        }
-
-        public static void Dumpper<T>(this Dictionary<string, T> stringDictonary) where T : struct
+        public static void Dump<T>(this Dictionary<string, T> stringDictonary) where T : struct
         {
 
         }
