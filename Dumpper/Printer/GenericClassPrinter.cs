201b3340276fb553358ff07b73ec927199defafe
purkayasta <pritompurkayasta@gmail.com>
2022-01-21 22:06:51 +0600

    Added interface support

--- Dumpper/Printer/GenericClassPrinter.cs
+++ Dumpper/Printer/GenericClassPrinter.cs
@@ -3,61 +3,59 @@ using Spectre.Console;
 
 namespace Dumpper.Printer
 {
-	internal class GenericClassPrinter
-	{
-		internal static void Print(PropertyInfo[] properties, string className)
-		{
-			Tree tree = GenerateTree(properties, className);
-			AnsiConsole.Write(tree);
-		}
+    internal class GenericClassPrinter
+    {
+        internal static void Print(PropertyInfo[] properties, string className)
+        {
+            Tree tree = GenerateTree(properties, className);
+            AnsiConsole.Write(tree);
+        }
 
-		internal static Tree GenerateTree(PropertyInfo[] properties, string className)
-		{
-			var tree = new Tree(className).Style("Red");
-			foreach (var property in properties)
-			{
-				tree.AddNodes(new Markup($"[bold white]{property.Name}[/]"));
-			}
+        internal static Tree GenerateTree(PropertyInfo[] properties, string className)
+        {
+            var tree = new Tree(className).Style("Red");
+            foreach (var property in properties) tree.AddNodes(new Markup($"[bold white]{property.Name}[/]"));
 
-			return tree;
-		}
+            return tree;
+        }
 
-		internal static void Print<T>(PropertyInfo[] properties, string className, T instance)
-		{
-			var tree = new Tree(className).Style("Yellow1");
-			foreach (var property in properties)
-			{
-				tree.AddNodes(new Markup($"[bold green1]{property?.Name} [/] : {property?.GetValue(instance, null)}"));
-			}
-			AnsiConsole.Write(tree);
-		}
+        internal static void Print<T>(PropertyInfo[] properties, string className, T instance)
+        {
+            var tree = new Tree(className).Style("Yellow1");
+            foreach (var property in properties)
+            {
+                tree.AddNodes(new Markup($"[bold green1]{property?.Name} [/] : {property?.GetValue(instance, null)}"));
+            }
 
-		internal static void PrintList<T>(PropertyInfo[] classProperties, IEnumerable<T> instances)
-		{
-			var table = new Table();
+            AnsiConsole.Write(tree);
+        }
 
-			foreach (var prop in classProperties)
-				table.AddColumn(new TableColumn($"[green]{prop.Name}[/]").Centered());
+        internal static void PrintList<T>(PropertyInfo[] classProperties, IEnumerable<T> instances)
+        {
+            var table = new Table();
 
-			string[] columnValues = new string[classProperties.Length];
+            foreach (var prop in classProperties)
+                table.AddColumn(new TableColumn($"[green]{prop.Name}[/]").Centered());
 
-			foreach (var instance in instances)
-			{
-				for (int i = 0; i < classProperties.Length; i++)
-				{
-					var values = classProperties[i].GetValue(instance);
-					if (values is null)
-						columnValues[i] = string.Empty;
-					else
-						columnValues[i] = values.ToString();
-				}
+            string[] columnValues = new string[classProperties.Length];
 
-				table.AddRow(columnValues);
+            foreach (var instance in instances)
+            {
+                for (int i = 0; i < classProperties.Length; i++)
+                {
+                    var values = classProperties[i].GetValue(instance);
+                    if (values is null)
+                        columnValues[i] = string.Empty;
+                    else
+                        columnValues[i] = values.ToString();
+                }
 
-				Array.Clear(columnValues, 0, columnValues.Length);
-			}
+                table.AddRow(columnValues);
 
-			AnsiConsole.Write(table);
-		}
-	}
+                Array.Clear(columnValues, 0, columnValues.Length);
+            }
+
+            AnsiConsole.Write(table);
+        }
+    }
 }
--- Dumpper/Types/Collections/Generics/ClassTypes.cs
+++ Dumpper/Types/Collections/Generics/ClassTypes.cs
@@ -2,90 +2,104 @@
 
 namespace Dumpper.Types.Collections
 {
-	public static class ClassTypes
-	{
-		public static void Dump<T>(this T instance) where T : class
-		{
-			var className = typeof(T).Name;
+    public static class ClassTypes
+    {
+        public static void Dump<T>(this T instance) where T : class
+        {
+            var className = typeof(T).Name;
 
-			if (instance == null)
-			{
-				instance = (T)Activator.CreateInstance(typeof(T));
-				GenericClassPrinter.Print(instance.GetType().GetProperties(), className);
-				PrimitiveValuePrinter.PrintLine($"Empty instances {className}");
-				return;
-			}
-			var type = instance.GetType();
+            if (instance == null)
+            {
+                if (IsItInterface(typeof(T))) return;
 
-			if (IsPrimitiveCollection(type, instance))
-				return;
+                instance = (T)Activator.CreateInstance(typeof(T));
+                GenericClassPrinter.Print(instance.GetType().GetProperties(), className);
+                PrimitiveValuePrinter.PrintLine($"Empty instances {className}");
+                return;
+            }
+            var type = instance.GetType();
 
-			var properties = type.GetProperties();
-			GenericClassPrinter.Print(properties, className, instance);
-		}
+            if (IsPrimitiveCollection(type, instance))
+                return;
 
-		public static void Dump<T>(this List<T> instances) where T : class
-		{
-			if (instances.Count < 1)
-			{
-				PrimitiveValuePrinter.PrintLine("No Value Found");
-				return;
-			}
+            var properties = type.GetProperties();
+            GenericClassPrinter.Print(properties, className, instance);
+        }
 
-			var type = instances[0].GetType();
+        public static void Dump<T>(this List<T> instances) where T : class
+        {
+            if (instances.Count < 1)
+            {
+                PrimitiveValuePrinter.PrintLine("No Value Found");
+                return;
+            }
 
-			foreach (var item in instances)
-			{
-				if (!IsPrimitiveCollection(type, item)) break;
-			}
+            var type = instances[0].GetType();
 
-			var properties = type.GetProperties();
-			GenericClassPrinter.PrintList(properties, instances);
-		}
+            foreach (var item in instances)
+            {
+                if (!IsPrimitiveCollection(type, item)) break;
+            }
 
-		private static bool IsPrimitiveCollection<T>(Type type, T instance)
-		{
-			var underlyingBaseType = type.UnderlyingSystemType;
-			var underlyingSystemName = type.UnderlyingSystemType.Name;
+            var properties = type.GetProperties();
+            GenericClassPrinter.PrintList(properties, instances);
+        }
 
-			if (underlyingSystemName.Contains("Int"))
-			{
-				PrimitiveArrayPrinter.Print(instance as int[]);
-				return true;
-			}
+        private static bool IsPrimitiveCollection<T>(Type type, T instance)
+        {
+            var underlyingBaseType = type.UnderlyingSystemType;
+            var underlyingSystemName = type.UnderlyingSystemType.Name;
 
-			if (underlyingSystemName.Contains("String"))
-			{
-				PrimitiveArrayPrinter.Print(instance as string[]);
-				return true;
-			}
+            if (underlyingSystemName.Contains("Int"))
+            {
+                PrimitiveArrayPrinter.Print(instance as int[]);
+                return true;
+            }
 
-			if (underlyingSystemName.Contains("List"))
-			{
-				var typeArguments = underlyingBaseType.UnderlyingSystemType.GetGenericArguments();
-				if (typeArguments.Length > 0)
-				{
-					var convertionType = typeArguments[0];
+            if (underlyingSystemName.Contains("String"))
+            {
+                PrimitiveArrayPrinter.Print(instance as string[]);
+                return true;
+            }
 
-					if (convertionType.Name.Contains("Int"))
-					{
-						var converted = instance as List<int>;
-						if (converted.Count > 0)
-							PrimitiveArrayPrinter.Print(converted.ToArray());
-					}
+            if (underlyingSystemName.Contains("List"))
+            {
+                var typeArguments = underlyingBaseType.UnderlyingSystemType.GetGenericArguments();
+                if (typeArguments.Length > 0)
+                {
+                    var convertionType = typeArguments[0];
 
-					if (convertionType.Name.Contains("String"))
-					{
-						var converted = instance as List<string>;
-						if (converted.Count > 0)
-							PrimitiveArrayPrinter.Print(converted.ToArray());
-					}
-				}
+                    if (convertionType.Name.Contains("Int"))
+                    {
+                        var converted = instance as List<int>;
+                        if (converted.Count > 0)
+                            PrimitiveArrayPrinter.Print(converted.ToArray());
+                    }
 
-				return true;
-			}
+                    if (convertionType.Name.Contains("String"))
+                    {
+                        var converted = instance as List<string>;
+                        if (converted.Count > 0)
+                            PrimitiveArrayPrinter.Print(converted.ToArray());
+                    }
+                }
 
-			return false;
-		}
-	}
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool IsItInterface(Type type)
+        {
+            if (!type.IsInterface) return false;
+
+            var methods = type.GetMethods();
+            var properties = type.GetProperties();
+
+            InterfacePrinter.Print(methods, properties, type.Name);
+
+            return true;
+        }
+    }
 }
